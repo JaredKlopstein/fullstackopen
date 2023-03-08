9ea2391702a1f7b72b3473e5b3418305f4021cfc
Jared <jaredklopstein@gmail.com>
2023-03-08 09:52:45 -0600

    step 2.3

--- part2/courseinfo/src/App.js
+++ part2/courseinfo/src/App.js
@@ -1,12 +1,15 @@
-const Course = ({course}) => (
-  <>  
+const Course = ({course}) => {
+  const total = course.parts.reduce((sum, part) => sum + part.exercises, 0)
+  
+  return (
+    <>  
   <h1>{course.name}</h1>
     {course.parts.map(part => 
       <p key={part.id}> {part.name} {part.exercises}</p>
-    )}
-    <b>There are a total of {course.parts[0].exercises + course.parts[1].exercises + course.parts[2].exercises} exercises</b>
+      )}
+    <b>There are a total of {total} exercises</b>
   </>
-)
+)}
 
 const App = () => {
   const course = {
@@ -30,6 +33,8 @@ const App = () => {
       }
     ]
   }
+
+  // const total = parts.reduce((s, p) => someMagicHere)
 
 
   return (
