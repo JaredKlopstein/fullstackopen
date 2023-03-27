3c8557600da8f2e5b8b886795704f5087fc45670
Jared <jaredklopstein@gmail.com>
2023-03-27 09:36:36 -0500

    exercise 4.4

--- part4/tests/list.test.js
+++ part4/tests/list.test.js
@@ -6,3 +6,44 @@ test('dummy returns one', () => {
   const result = listHelper.dummy(blogs)
   expect(result).toBe(1)
 })
+
+describe('total likes', () => {
+    const listWithOneBlog = [
+        {
+          _id: '5a422aa71b54a676234d17f8',
+          title: 'Go To Statement Considered Harmful',
+          author: 'Edsger W. Dijkstra',
+          url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+          likes: 5,
+          __v: 0
+        }
+      ]
+      const listWithTwoBlogs = [
+        {
+          _id: '5a422aa71b54a676234d17f8',
+          title: 'Go To Statement Considered Harmful',
+          author: 'Edsger W. Dijkstra',
+          url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+          likes: 5,
+          __v: 0
+        },
+        {
+            _id: '5a422aa71b54a676234d17f2',
+            title: 'Go To Statement Considered Harmful V2',
+            author: 'Edsger W. Dijkstra',
+            url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+            likes: 6,
+            __v: 0
+          }
+      ]
+
+    test('of empty list is zero', () => {
+      expect(listHelper.total([])).toBe(0)
+    })
+    test('when list has only one blog equals the likes of that', () => {
+      expect(listHelper.total(listWithOneBlog)).toBe(5)
+    })
+    test('of a bigger list is calculated right', () => {
+      expect(listHelper.total(listWithTwoBlogs)).toBe(11)
+    })
+})
--- part4/utils/list_helper.js
+++ part4/utils/list_helper.js
@@ -1,7 +1,21 @@
 const dummy = (blogs) => {
     return 1
   }
+
+const total = (array) => {
+    const reducer = (sum, item) => {
+      return sum + item.likes
+    }
+  
+    return array.length === 0
+      ? 0
+      : array.reduce(reducer, 0)
+  }
+
+
   
   module.exports = {
-    dummy
+    dummy,
+    total
   }
+
